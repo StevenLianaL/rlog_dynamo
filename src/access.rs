15598aa1c39bfd511776e2b6da9388cd30b7cf21
wz <wangzhou8284@163.com>
2025-07-31 17:22:25 +0800

    v0.4: add project

--- src/access.rs
+++ src/access.rs
@@ -38,6 +38,7 @@ impl LogAccess {
         expression_attribute_names.insert("#c".to_string(), "count".to_string());
         expression_attribute_names.insert("#a".to_string(), "app".to_string());
         expression_attribute_names.insert("#m".to_string(), "month".to_string());
+        expression_attribute_names.insert("#p".to_string(), "project".to_string());
 
         // 构建 ExpressionAttributeValues
         let mut expression_attribute_values = HashMap::new();
@@ -46,13 +47,14 @@ impl LogAccess {
         expression_attribute_values.insert(":inc".to_string(), AttributeValue::N("1".to_string()));
         expression_attribute_values.insert(":app".to_string(), AttributeValue::S(app.to_string()));
         expression_attribute_values.insert(":month".to_string(), AttributeValue::S(month_part.to_string()));
+        expression_attribute_values.insert(":project".to_string(), AttributeValue::S(self.project.to_string()));
 
         // 执行 update_item
         self.client
             .update_item()
             .table_name(self.table_name)
             .set_key(Some(key))
-            .update_expression("SET #c = if_not_exists(#c, :start) + :inc, #a = :app, #m = :month")
+            .update_expression("SET #c = if_not_exists(#c, :start) + :inc, #a = :app, #m = :month, #p = :project")
             .set_expression_attribute_names(Some(expression_attribute_names))
             .set_expression_attribute_values(Some(expression_attribute_values))
             .return_values(ReturnValue::UpdatedNew)
