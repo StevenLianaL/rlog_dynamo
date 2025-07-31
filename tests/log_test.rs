e360a318431b5b6942ac0c803850a7defa055eaa
wz <wangzhou8284@163.com>
2025-07-31 14:35:32 +0800

    update log format

--- tests/log_test.rs
+++ tests/log_test.rs
@@ -3,7 +3,7 @@ use rlog_dynamo::{debug, error, info, warning};
 #[tokio::test]
 async fn test_log_macro() {
     dotenv::dotenv().ok();
-    let access_key =dotenv::var("AWS_ACCESS_KEY").unwrap();
+    let access_key = dotenv::var("AWS_ACCESS_KEY").unwrap();
     let secret_key = dotenv::var("AWS_SECRET_KEY").unwrap();
     let logger = rlog_dynamo::record::DynamoLogger::new(
         "rlog",
@@ -15,8 +15,8 @@ async fn test_log_macro() {
         "log_record",
     );
 
-    info!(logger, "info msg", 0).await;
-    warning!(logger, "warning msg", 0).await;
-    error!(logger, "error msg", 0).await;
-    debug!(logger, "debug msg", 0).await;
+    info!(&logger, "info msg", 0).await;
+    warning!(&logger, "warning msg", 0).await;
+    error!(&logger, "error msg", 0).await;
+    debug!(&logger, "debug msg", 0).await;
 }
